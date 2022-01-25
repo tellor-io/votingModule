6d0df2fa2e09c80dc83e9617b5ce832a5b16e639
Abdullah <abady1000@gmail.com>
2022-01-25 10:21:32 +0300

    Arranging the header of the page and fix its responsivness by modifying Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,23 @@
 import React, { useContext } from "react";
 import "../styles/Nav.css";
-import { ReactComponent as TellorGrey } from "../assets/Tellor.svg";
+// import { ReactComponent as TellorGrey } from "../assets/Tellor.svg";
+import LogoMaker from "./LogoMaker";
 import { AppContext } from "../index";
 import { truncateAddr } from "../utils/helpers";
 import { Jazzicon } from "@ukstv/jazzicon-react";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css"; // optional
 
+// const LogoMaker = require('./LogoMaker').default;
+
 function Nav({ currAddr }) {
   //Context
   const data = useContext(AppContext);
-
   return (
     <div className="NavBar">
       <div className="NavBar__Left">
-        <TellorGrey className="NavBar__Title" />
+        {/* <TellorGrey className="NavBar__Title" /> */}
+        <LogoMaker/>
       </div>
       <div className="NavBar__Right">
         <Tippy content="To switch networks, use MetaMask extension.">
