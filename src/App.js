fac491d3707f6c52fb951df30f289cff1554d77a
Abdullah <abady1000@gmail.com>
2022-02-21 06:54:16 +0300

    Main component is moved from App component to Vote.
    App now is a routing table

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 //Router
-import { BrowserRouter as Router } from "react-router-dom";
-//Components
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-//Web3
-import { ethers } from "ethers";
-//Context
-import { AppContext } from "./index";
+import {Route, Routes } from "react-router-dom";
+
+                   
+import Vote from "./components/Vote";
+import PleaseConnect from "./components/PleaseConnect";
+import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
-  //Component State
-  const [currAddr, setCurrAddr] = useState("");
-  const [signer, setSigner] = useState({});
-  //Context
-  const data = useContext(AppContext);
-  //Globals
-  //Listening for changes in ChainId (Mainnet/Rinkeby/Others)
-  window.ethereum.on("chainChanged", () => {
-    window.location.reload();
-  });
-  //Listening for changes in Metamask Accounts
-  window.ethereum.on("accountsChanged", (accounts) => {
-    setCurrAddr(ethers.utils.getAddress(accounts[0]));
-    const signerFromProvider = data.provider.getSigner();
-    setSigner(signerFromProvider);
-  });
-
+  
   return (
     <div className="App">
-      <Router>
-        
-      <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
-        <Nav currAddr={currAddr} />
-        <Hero currAddr={currAddr} signer={signer} />
-        <Footer />
-      </Router>
+      <Routes>
+        <Route path='/' element={<PleaseConnect/>}/>
+        <Route path='/vote' element={<Vote/>}/>
+      </Routes>
     </div>
   );
 }
