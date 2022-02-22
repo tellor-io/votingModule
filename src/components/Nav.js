22abee46a40f6442ad38242053c46613792806be
Abdullah <abady1000@gmail.com>
2022-02-22 19:10:37 +0300

    Some modifications to let AppContext works better

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "../styles/Nav.css";
 // import { ReactComponent as TellorGrey } from "../assets/Tellor.svg";
 import LogoMaker from "./LogoMaker";
-import { AppContext } from "../index";
+import { web3Context } from "../App";
 import { truncateAddr } from "../utils/helpers";
 import { Jazzicon } from "@ukstv/jazzicon-react";
 import Tippy from "@tippyjs/react";
@@ -11,8 +11,11 @@ import "tippy.js/dist/tippy.css"; // optional
 // const LogoMaker = require('./LogoMaker').default;
 
 function Nav({ currAddr }) {
+  
   //Context
-  const data = useContext(AppContext);
+  const data = useContext(web3Context);
+  
+  // address={currAddr.length > 0 ? currAddr : data.currentAddress}
   return (
     <div className="NavBar">
       <div className="NavBar__Left">
@@ -30,11 +33,11 @@ function Nav({ currAddr }) {
             {`User: ${
               currAddr.length > 0
                 ? truncateAddr(currAddr)
-                : truncateAddr(data.currentAddress)
+                : truncateAddr(data.address)
             }`}
             <div className="NavBar__UserIconContainer">
               <Jazzicon
-                address={currAddr.length > 0 ? currAddr : data.currentAddress}
+                address={data.address}
                 className="NavBar__UserIcon"
               />
             </div>
