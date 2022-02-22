a6a018b98f33c6687b1892d2d2684edc0eb7f58f
Abdullah <abady1000@gmail.com>
2022-02-22 19:07:22 +0300

    Context added to App, so it is capable of managing data easier.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, {createContext} from "react";
 //Router
 import {Route, Routes } from "react-router-dom";
 
                    
 import Vote from "./components/Vote";
 import PleaseConnect from "./components/PleaseConnect";
-import ProtectedRoute from "./components/protectedRoute";
+
+const web3Context = createContext({
+  web3: null,
+  chainId: 0x0,
+  address: 0x0,
+  error: false
+})
 
 function App() {
-  
+
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<PleaseConnect/>}/>
-        <Route path='/vote' element={<Vote/>}/>
-      </Routes>
+      <web3Context.Provider value={{web3: null, chainId: 0x0, address: 0x0, error: false}}>
+        <Routes>
+          <Route path='/' element={<PleaseConnect/>}/>
+          <Route path='/vote' element={<Vote/>}/>
+        </Routes>
+      </web3Context.Provider>
     </div>
   );
 }
 
-export default App;
+export {
+  App,
+  web3Context
+}
+
