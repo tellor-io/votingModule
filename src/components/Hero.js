61d3dab17c98b89162f06e43ba50c95ed8196307
Abdullah <abady1000@gmail.com>
2022-02-22 19:10:03 +0300

    Some modifications to let app context work.
    Fix warning in Time table.
    Just added <Table.Body>

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import "../styles/Hero.css";
 //Web3
 import { ethers } from "ethers";
 //Context
-import { AppContext } from "../index";
+import { web3Context } from "../App";
 //Utils
 import TellorGovABI from "../utils/TellorGovABI.json";
 //Components
@@ -20,7 +20,7 @@ function Hero({ currAddr, signer }) {
     const [errMessage, setErrMessage] = useState(null);
     const [txnHash, setTxnHash] = useState(null);
     //Context
-    const data = useContext(AppContext);
+    const data = useContext(web3Context);
     //Globals
     const voteIdMainnet = 3; //current
     const voteIdRinkeby = 9; //current
@@ -201,22 +201,26 @@ function Hero({ currAddr, signer }) {
                                 <h2 className="Hero__Duration">Duration</h2>
                                 <div className="Hero__duration_container_table">
                                     <Table definition>
-                                        <Table.Row>
-                                            <Table.Cell>
-                                                <label className='Hero__row_header'>
-                                                    Open
-                                                </label>
-                                            </Table.Cell>
-                                            <Table.Cell className="Hero__date_font">January 14th, 2022 @ 2pm EST</Table.Cell>
-                                        </Table.Row>
-                                        <Table.Row>
-                                            <Table.Cell>
-                                                <label className='Hero__row_header'>
-                                                    Close
-                                                </label>
-                                            </Table.Cell>
-                                            <Table.Cell className="Hero__date_font">January 21st, 2022 @ 2pm EST</Table.Cell>
-                                        </Table.Row>
+                                        <Table.Header>
+                                            <Table.Row>
+                                                <Table.Cell>
+                                                    <label className='Hero__row_header'>
+                                                        Open
+                                                    </label>
+                                                </Table.Cell>
+                                                <Table.Cell className="Hero__date_font">January 14th, 2022 @ 2pm EST</Table.Cell>
+                                            </Table.Row>
+                                        </Table.Header>
+                                        <Table.Body>
+                                            <Table.Row>
+                                                <Table.Cell>
+                                                    <label className='Hero__row_header'>
+                                                        Close
+                                                    </label>
+                                                </Table.Cell>
+                                                <Table.Cell className="Hero__date_font">January 21st, 2022 @ 2pm EST</Table.Cell>
+                                            </Table.Row>
+                                        </Table.Body>
                                     </Table>           
                                 </div>
                             </div>
