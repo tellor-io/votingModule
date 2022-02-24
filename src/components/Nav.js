072ac227dbaab2fee853703e329c0a25add05970
Abdullah <abady1000@gmail.com>
2022-02-25 01:20:35 +0300

    fix imports and modify text

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "../styles/Nav.css";
 // import { ReactComponent as TellorGrey } from "../assets/Tellor.svg";
 import LogoMaker from "./LogoMaker";
@@ -10,39 +10,41 @@ import "tippy.js/dist/tippy.css"; // optional
 
 // const LogoMaker = require('./LogoMaker').default;
 
-function Nav({ currAddr }) {
+function Nav({currAddr, chainId}) {
   
   //Context
   const data = useContext(web3Context);
+  // const [chainId, setChainId] = useState(chainid);
   
-  // address={currAddr.length > 0 ? currAddr : data.currentAddress}
   return (
-    <div className="NavBar">
-      <div className="NavBar__Left">
-        {/* <TellorGrey className="NavBar__Title" /> */}
-        <LogoMaker/>
-      </div>
-      <div className="NavBar__Right">
-        <Tippy content="To switch networks, use MetaMask extension.">
-          <h1 className="NavBar__Network">{`Network: ${
-            data.chainId.toString(16) === "1" ? "Mainnet" : "Rinkeby"
-          }`}</h1>
-        </Tippy>
-        <Tippy content="To change your address, use MetaMask extension.">
-          <div className="NavBar__Address">
-            {`User: ${
-              currAddr.length > 0
-                ? truncateAddr(currAddr)
-                : truncateAddr(data.address)
-            }`}
-            <div className="NavBar__UserIconContainer">
-              <Jazzicon
-                address={data.address}
-                className="NavBar__UserIcon"
-              />
+    <div>
+      <div className="NavBar">
+        <div className="NavBar__Left">
+          {/* <TellorGrey className="NavBar__Title" /> */}
+          <LogoMaker/>
+        </div>
+        <div className="NavBar__Right">
+          <Tippy content="To switch networks, use yout wallet.">
+            <h1 className="NavBar__Network">{`Network: ${
+              chainId === "0x1" ? "Mainnet" : "Rinkeby"
+            }`}</h1>
+          </Tippy>
+          <Tippy content="To change your address, use your wallet.">
+            <div className="NavBar__Address">
+              {`User: ${
+                currAddr.length > 0
+                  ? truncateAddr(currAddr)
+                  : truncateAddr(data.address)
+              }`}
+              <div className="NavBar__UserIconContainer">
+                <Jazzicon
+                  address={data.address}
+                  className="NavBar__UserIcon"
+                />
+              </div>
             </div>
-          </div>
-        </Tippy>
+          </Tippy>
+        </div>
       </div>
     </div>
   );
