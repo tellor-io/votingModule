1d546fcec88b4b0bd16c8a6bd19a6173d3214973
Abdullah <abady1000@gmail.com>
2022-02-25 01:22:07 +0300

    Enhance managing page logic

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,53 +1,30 @@
 
-import React, { useState, useContext, useEffect } from "react";
+import React, {useState, useContext} from "react";
 //Router
 import { BrowserRouter as Router, useNavigate, Navigate } from "react-router-dom";
 //Components
 import Footer from "./Footer";
 import Nav from "./Nav";
 import Hero from "./Hero";
+import {ethers} from 'ethers';
 
 //Context
 import { web3Context } from "../App";
 
-
 function Vote(){
-    
-    const navigate = useNavigate();
+
     const data = useContext(web3Context)
 
-    console.log(`Data: ${Object.values(data)}`);
+    const [chainId, setChainId] = useState(`0x${data.chainId.toString(16)}`);
+    const [address, setAddress] = useState(data.address);
+    const [signer, setSigner] = useState(data.signer);
     
-    // useEffect(()=>{
-    //      console.log("inside effect");
-    //     if(data === null || data.web3 === null || data.web3 === undefined){
-    //         data.error = true;
-    //         data.errorCode = 'setupweb3';
-    //         navigate('/');
-    //         return;
-    //     }
-    // }, []);
+    const navigate = useNavigate();
+
     
-    //Component State
-    // const [currAddr, setCurrAddr] = useState("");
-    // const [signer, setSigner] = useState({});
-    //Context
-    // const data = useContext(AppContext);
-    //Globals
+    // console.log(`Vote:web3: ${}`)
 
-    //Listening for changes in ChainId (Mainnet/Rinkeby/Others)
-    // window.ethereum.on("chainChanged", (data) => {
-    //     window.location.reload();
-    // });
-
-    console.log("Found Ethereum");
-
-    //Listening for changes in Metamask Accounts
-    // window.ethereum.on("accountsChanged", (accounts) => {
-    //     setCurrAddr(ethers.utils.getAddress(accounts[0]));
-    //     const signerFromProvider = data.provider.getSigner();
-    //     setSigner(signerFromProvider);
-    // });
+   
 
     // currAddr = 0;
     // signer = 0;
@@ -59,10 +36,25 @@ function Vote(){
         );
     }
     else{      
+
+        data.web3.getNetwork().then((network)=>{
+            console.log(`inside: ${network.chainId}`);
+        })
+
+        data.provider.on("chainChanged", (chainid) => {
+            window.location.reload();
+        });
+        
+        data.provider.on("accountsChanged", (account) => {
+            window.location.reload();
+        });
+        
+        console.log(`Address: ${address}`);
+
         return (
         <div className="App">
-            <Nav currAddr={data.address} />
-            <Hero currAddr={data.address} signer={data.signer} />
+            <Nav currAddr={address} chainId={chainId} />
+            <Hero currAddr={address} signer={signer} />
             <Footer />
         </div>
         );
